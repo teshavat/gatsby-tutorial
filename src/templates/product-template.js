161b5f58a7c8154458782452ae8a96bf8890d582
Teshavat Tantiviwat <teshavat@Teshavats-MacBook-Pro.local>
2019-05-17 09:14:24 +0700

    title changed

--- src/templates/product-template.js
+++ src/templates/product-template.js
@@ -11,7 +11,7 @@ export default function ({data}) {
 
   return (
       <Layout>
-          <h1>Single Product Template</h1>
+          <h1 style={{ color: 'red' }}>Single Product Template</h1>
           <Img fluid={fluid} />
           <h1>{title} <span style={{marginLeft:"2rem",color:'grey'}}>${price}</span></h1>
           <p>{description}</p>
